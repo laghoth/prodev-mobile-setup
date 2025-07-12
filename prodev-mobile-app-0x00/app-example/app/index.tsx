e8be1e50706a695c4ccd8895655ae0c7c501aa93
OTHMANE LAGHLIMI <laghlim.othmane@gmail.com>
2025-07-12 15:18:26 +0100

    commit 6

--- prodev-mobile-app-0x00/app-example/app/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/index.tsx
@@ -7,9 +7,10 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "green",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>first app</Text>
     </View>
   );
 }
